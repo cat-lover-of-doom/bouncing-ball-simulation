e8b7299ba2704cd10e1d90d7d2964e7c047d9609
cat-lover-of-doom <91356335+cat-lover-of-doom@users.noreply.github.com>
2026-01-26 15:15:08 -0500

    added ball type

--- src/main.c
+++ src/main.c
@@ -1,40 +1,73 @@
 #include "raylib.h"
+#include <assert.h>
 
-#define SCREEN_WIDTH 800
-#define SCREEN_HEIGHT 450
+constexpr int SCREEN_WIDTH = 800;
+constexpr int SCREEN_HEIGHT = 450;
 
-int main(void)
-{
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Simple Raylib Module");
+typedef struct ball {
+  Vector2 position;
+  Vector2 speed;
+  Vector2 acceleration;
+  float radius;
+} Ball;
 
-    SetTargetFPS(60);
+float Clamp(float d, float min, float max) {
+  const float t = d < min ? min : d;
+  return t > max ? max : t;
+}
 
-    Vector2 ballPosition = { (float)SCREEN_WIDTH/2, (float)SCREEN_HEIGHT/2 };
-    Vector2 ballSpeed = { 6.0f, 60.0f };
-    float ballRadius = 10.0f;
+int main(void) {
 
-    while (!WindowShouldClose())
-    {
-        ballPosition.x += ballSpeed.x;
-        ballPosition.y += ballSpeed.y;
+  InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Simple Raylib Module");
 
-        if ((ballPosition.x >= (SCREEN_WIDTH - ballRadius)) || (ballPosition.x <= ballRadius))
-            ballSpeed.x *= -1.0f;
-        if ((ballPosition.y >= (SCREEN_HEIGHT - ballRadius)) || (ballPosition.y <= ballRadius))
-            ballSpeed.y *= -1.0f;
+  SetTargetFPS(60);
 
-        BeginDrawing();
+  Ball ball = {.position = {(float)SCREEN_WIDTH / 2, (float)SCREEN_HEIGHT / 2},
+               .speed = {6.0f, 60.0f},
+               .radius = 10.0f};
 
-            ClearBackground(RAYWHITE);
+  while (!WindowShouldClose()) {
+    if (IsKeyPressed(KEY_A)) {
+      ball.radius *= 2;
+    }
+    ball.position.x += ball.speed.x;
+    ball.position.y += ball.speed.y;
 
-            DrawCircleV(ballPosition, ballRadius, MAROON);
-            DrawText("Simple Raylib Example - Bouncing Ball", 10, 10, 20, DARKGRAY);
-            DrawFPS(10, 40);
-
-        EndDrawing();
+    if (ball.position.x - ball.radius < 0 ||
+        ball.position.x + ball.radius > GetScreenWidth()) {
+      ball.speed.x *= -1.0f;
+      ball.position.x =
+          Clamp(ball.position.x, ball.radius, GetScreenWidth() - ball.radius);
     }
 
-    CloseWindow();
+    if (ball.position.y - ball.radius < 0 ||
+        ball.position.y + ball.radius > GetScreenHeight()) {
+      ball.speed.y *= -1.0f;
+      ball.position.y =
+          Clamp(ball.position.y, ball.radius, GetScreenHeight() - ball.radius);
+    }
 
-    return 0;
+    // TODO: fix this shit
+    // assert(ballPosition.x >= ballRadius && "Ball X position is out of left
+    // bound"); assert(ballPosition.x <= GetScreenWidth() - ballRadius && "Ball
+    // X position is out of right bound"); assert(ballPosition.y >= ballRadius
+    // && "Ball Y position is out of top bound"); assert(ballPosition.y <=
+    // GetScreenHeight() - ballRadius && "Ball Y position is out of bottom
+    // bound");
+
+    BeginDrawing();
+
+    ClearBackground(RAYWHITE);
+
+    // DrawRectangleV(ballPosition, (Vector2){10.f, 10.f}, MAROON);
+    DrawCircleV(ball.position, ball.radius, MAROON);
+    DrawText("Simple Raylib Example - Bouncing Ball", 10, 10, 20, DARKGRAY);
+    DrawFPS(10, 40);
+
+    EndDrawing();
+  }
+
+  CloseWindow();
+
+  return 0;
 }

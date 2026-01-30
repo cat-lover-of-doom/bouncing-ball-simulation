e1c058c294fda5223fbcef9d4502332cabbbf41b
cat-lover-of-doom <91356335+cat-lover-of-doom@users.noreply.github.com>
2026-01-29 19:27:29 -0500

    added the acumulator pattern

--- src/main.c
+++ src/main.c
@@ -3,43 +3,51 @@
 
 constexpr int INITIAL_SCREEN_WIDTH = 800;
 constexpr int INITIAL_SCREEN_HEIGHT = 450;
-constexpr int TARGET_FPS = 60;
-constexpr double DELTAT = 1. / TARGET_FPS;
+constexpr double FRAMETIME_MAX = .25f;
+constexpr double SIM_DT = 1./12000;
 
 typedef struct ball {
     Vector2 position;
-    Vector2 speed;
+    Vector2 velocity;
     Vector2 acceleration;
-    float radius;
+    double radius;
 } Ball;
 
-float Clamp(float d, float min, float max) {
-    const float t = d < min ? min : d;
+double Clamp(double d, double min, double max) {
+    const double t = d < min ? min : d;
     return t > max ? max : t;
 }
 void moveBall(Ball *ball);
 bool wallColision(Ball *ball);
-float meterToPixel(float m);
-float pixelToMeter(float p);
+double meterToPixel(double m);
+double pixelToMeter(double p);
 
 int main(void) {
 
-    InitWindow(INITIAL_SCREEN_WIDTH, INITIAL_SCREEN_HEIGHT,
-               "Simple Raylib Module");
-
-    SetTargetFPS(TARGET_FPS);
+    InitWindow(INITIAL_SCREEN_WIDTH, INITIAL_SCREEN_HEIGHT, "BALL");
+    SetTargetFPS(120);
 
     Ball ball = {.position = {400.0f, 225.0f},
-                 .speed = {200.0f, 200.0f},
-                 .acceleration = {0.0f, 3000.0f},
+                 .velocity = {200.0f, 200.0f},
+                 .acceleration = {0.0f, meterToPixel(9.8f)},
                  .radius = 20.0f};
+
+    double simTime = 0.f;
 
     while (!WindowShouldClose()) {
         if (IsKeyPressed(KEY_A)) {
             ball.radius *= 2;
         }
-        moveBall(&ball);
-        wallColision(&ball);
+        double frametime = GetFrameTime();
+        if (frametime > FRAMETIME_MAX) {
+            frametime = FRAMETIME_MAX;
+        }
+        simTime += frametime;
+        while (simTime >= SIM_DT) {
+            moveBall(&ball);
+            wallColision(&ball);
+            simTime-=SIM_DT;
+        }
 
         // TODO: fix this shit
         // assert(ballPosition.x >= ballRadius && "Ball X position is out of
@@ -56,7 +64,7 @@ int main(void) {
 
         // DrawRectangleV(ballPosition, (Vector2){10.f, 10.f}, MAROON);
         DrawCircleV(ball.position, ball.radius, MAROON);
-        DrawText("Simple Raylib Example - Bouncing Ball", 10, 10, 20, DARKGRAY);
+        DrawText("BALL GO BRRRR", 10, 10, 20, DARKGRAY);
         DrawFPS(10, 40);
 
         EndDrawing();
@@ -68,11 +76,11 @@ int main(void) {
 }
 
 void moveBall(Ball *ball) {
-    ball->speed.x += ball->acceleration.x * DELTAT;
-    ball->speed.y += ball->acceleration.y * DELTAT;
+    ball->velocity.x += ball->acceleration.x * SIM_DT;
+    ball->velocity.y += ball->acceleration.y * SIM_DT;
 
-    ball->position.x += ball->speed.x * DELTAT;
-    ball->position.y += ball->speed.y * DELTAT;
+    ball->position.x += ball->velocity.x * SIM_DT;
+    ball->position.y += ball->velocity.y * SIM_DT;
 }
 
 bool wallColision(Ball *ball) {
@@ -82,7 +90,7 @@ bool wallColision(Ball *ball) {
 
     if (ball->position.x - ball->radius < 0 ||
         ball->position.x + ball->radius > screenWidth) {
-        ball->speed.x *= -1.0f;
+        ball->velocity.x *= -1.0f;
         ball->position.x =
             Clamp(ball->position.x, ball->radius, screenWidth - ball->radius);
         colisionOcurred = true;
@@ -90,7 +98,7 @@ bool wallColision(Ball *ball) {
 
     if (ball->position.y - ball->radius < 0 ||
         ball->position.y + ball->radius > screenHeight) {
-        ball->speed.y *= -1.0f;
+        ball->velocity.y *= -1.0f;
         ball->position.y =
             Clamp(ball->position.y, ball->radius, screenHeight - ball->radius);
         colisionOcurred = true;
@@ -98,10 +106,6 @@ bool wallColision(Ball *ball) {
     return colisionOcurred;
 }
 
-float meterToPixel(float m){
-    return m*3779.5275591;
-}
+double meterToPixel(double m) { return m * 3779.5275591; }
 
-float pixelToMeter(float p){
-    return p*0.0002645833;
-}
+double pixelToMeter(double p) { return p * 0.0002645833; }
